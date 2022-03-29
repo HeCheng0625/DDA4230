2976dac5ba54471f654f5c1167341a6c7fb98a9b
Yuancheng Wang <119010319@link.cuhk.edu.cn>
2022-03-30 00:06:35 +0800

    first commit

---
+++ test.py
@@ -0,0 +1,13 @@
+# import random
+# import numpy as np
+# a = np.array([[1,2,3,4,5,6,7],[2,3,4,5,6,7,8]])
+# for i in range(10):
+#     print(random.choice(a[0]))
+
+# for i in range(100):
+#     if np.random.choice([0,1], p = [0.1, 0.9]):
+#         print(1)
+#     else:
+#         print(0)
+
+print(1+None)
